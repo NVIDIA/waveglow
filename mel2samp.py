63bdb3f4b958b93692fec10d6fa41478fdaacb5e
rafaelvalle <rafaelvalle@nvidia.com>
2018-11-09 13:28:20 -0800

    mel2samp.py: adding fmin and fmax params

--- mel2samp.py
+++ mel2samp.py
@@ -63,14 +63,15 @@ class Mel2Samp(torch.utils.data.Dataset):
     spectrogram, audio pair.
     """
     def __init__(self, training_files, segment_length, filter_length,
-                 hop_length, win_length, sampling_rate):
+                 hop_length, win_length, sampling_rate, mel_fmin, mel_fmax):
         self.audio_files = files_to_list(training_files)
         random.seed(1234)
         random.shuffle(self.audio_files)
         self.stft = TacotronSTFT(filter_length=filter_length,
                                  hop_length=hop_length,
                                  win_length=win_length,
-                                 sampling_rate=sampling_rate)
+                                 sampling_rate=sampling_rate,
+                                 mel_fmin=mel_fmin, mel_fmax=mel_fmax)
         self.segment_length = segment_length
         self.sampling_rate = sampling_rate
 
