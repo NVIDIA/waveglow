6be097735775b7f90d26e030d1c15152a9fbdf9e
rafaelvalle <rafaelvalle@nvidia.com>
2018-11-14 13:38:35 -0800

    glow_old.py: layout

--- glow_old.py
+++ glow_old.py
@@ -2,12 +2,13 @@ import copy
 import torch
 from glow import Invertible1x1Conv, remove
 
+
 @torch.jit.script
-def fused_add_tanh_sigmoid_multiply(input_a, input_b,n_channels):
+def fused_add_tanh_sigmoid_multiply(input_a, input_b, n_channels):
     n_channels_int = n_channels[0]
     in_act = input_a+input_b
     t_act = torch.nn.functional.tanh(in_act[:, :n_channels_int, :])
-    s_act = torch.nn.functional.sigmoid(in_act[:,n_channels_int:, :])
+    s_act = torch.nn.functional.sigmoid(in_act[:, n_channels_int:, :])
     acts = t_act * s_act
     return acts
 
