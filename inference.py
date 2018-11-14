044cc4aa11d9c6b379495b7a18b24a7243055a65
Chaitanya Talnikar <ctalnikar@nvidia.com>
2018-11-13 17:15:04 -0800

    Ensure remove weight norm actually removes the weight computation from network

--- inference.py
+++ inference.py
@@ -29,11 +29,12 @@ import numpy as np
 from scipy.io.wavfile import write
 import torch
 from mel2samp import files_to_list, MAX_WAV_VALUE
+from glow import remove_weightnorm
 
 def main(mel_files, waveglow_path, sigma, output_dir, sampling_rate, is_fp16):
     mel_files = files_to_list(mel_files)
     waveglow = torch.load(waveglow_path)['model']
-    waveglow.remove_weightnorm()
+    waveglow = remove_weightnorm(waveglow)
     waveglow.cuda().eval()
     if is_fp16:
         waveglow.half()
