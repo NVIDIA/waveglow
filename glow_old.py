d19cc9dd253484c12e09e3b9a07479c9bcf9b62f
rafaelvalle <rafaelvalle@nvidia.com>
2018-11-16 07:35:21 +0000

    glow_old.py: using fused res_skip, removing deep copy

--- glow_old.py
+++ glow_old.py
@@ -27,8 +27,7 @@ class WN(torch.nn.Module):
         self.n_layers = n_layers
         self.n_channels = n_channels
         self.in_layers = torch.nn.ModuleList()
-        self.res_layers = torch.nn.ModuleList()
-        self.skip_layers = torch.nn.ModuleList()
+        self.res_skip_layers = torch.nn.ModuleList()
         self.cond_layers = torch.nn.ModuleList()
 
         start = torch.nn.Conv1d(n_in_channels, n_channels, 1)
@@ -56,13 +55,12 @@ class WN(torch.nn.Module):
 
             # last one is not necessary
             if i < n_layers - 1:
-                res_layer = torch.nn.Conv1d(n_channels, n_channels, 1)
-                res_layer = torch.nn.utils.weight_norm(res_layer, name='weight')
-                self.res_layers.append(res_layer)
-
-            skip_layer = torch.nn.Conv1d(n_channels, n_channels, 1)
-            skip_layer = torch.nn.utils.weight_norm(skip_layer, name='weight')
-            self.skip_layers.append(skip_layer)
+                res_skip_channels = 2*n_channels
+            else:
+                res_skip_channels = n_channels
+            res_skip_layer = torch.nn.Conv1d(n_channels, res_skip_channels, 1)
+            res_skip_layer = torch.nn.utils.weight_norm(res_skip_layer, name='weight')
+            self.res_skip_layers.append(res_skip_layer)
 
     def forward(self, forward_input):
         audio, spect = forward_input
@@ -74,15 +72,17 @@ class WN(torch.nn.Module):
                 self.cond_layers[i](spect),
                 torch.IntTensor([self.n_channels]))
 
+            res_skip_acts = self.res_skip_layers[i](acts)
             if i < self.n_layers - 1:
-                res_acts = self.res_layers[i](acts)
-                audio = res_acts + audio
+                audio = res_skip_acts[:,:self.n_channels,:] + audio
+                skip_acts = res_skip_acts[:,self.n_channels:,:]
+            else:
+                skip_acts = res_skip_acts
 
             if i == 0:
-                output = self.skip_layers[i](acts)
+                output = skip_acts
             else:
-                output = self.skip_layers[i](acts) + output
-
+                output = skip_acts + output
         return self.end(output)
 
 
@@ -224,11 +224,10 @@ class WaveGlow(torch.nn.Module):
 
     @staticmethod
     def remove_weightnorm(model):
-        waveglow = copy.deepcopy(model)
+        waveglow = model
         for WN in waveglow.WN:
             WN.start = torch.nn.utils.remove_weight_norm(WN.start)
             WN.in_layers = remove(WN.in_layers)
             WN.cond_layers = remove(WN.cond_layers)
-            WN.res_layers = remove(WN.res_layers)
-            WN.skip_layers = remove(WN.skip_layers)
+            WN.res_skip_layers = remove(WN.res_skip_layers)
         return waveglow
