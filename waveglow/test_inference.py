d8ce48d6a28a8404829c0ad44c03dba3739beeca
なるみ <weaper@gmail.com>
2020-07-30 01:00:09 +0800

    Add testcase

---
+++ waveglow/test_inference.py
@@ -0,0 +1,9 @@
+from .inference import synthesize
+import torch
+
+
+def test_synthesize():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    mel = torch.randn(2, 80, 10)
+    audio = synthesize(mel, 'cpu')
+    assert list(audio.size()) == [2, 2560]
